c47d6c1069721e32aebf91e3edffafd202c6b617
JoshPastrana <s3944069@student.rmit.edu.au>
2024-09-30 02:00:24 +1000

    Update.html

--- assignment2/script.js
+++ assignment2/script.js
@@ -1,3 +1,5 @@
+const likeBtn = document.querySelector("#like-btn");
+const likesDisplay = document.querySelector("#likes");
 const video = document.querySelector("#custom-video-player");
 const playPauseBtn = document.querySelector("#play-pause-btn");
 const playPauseImg = document.querySelector("#play-pause-img");
@@ -9,18 +11,16 @@ const muteImg = document.querySelector("#mute-img");
 const replayBtn = document.querySelector("#replay-btn");
 const speedUpBtn = document.querySelector("#speed-up-btn");
 const speedUpImg = document.querySelector("#speed-up-img");
-// All od the const I put for the buttons to work ranging from the video player itself to the speed up buttons
+// All off the const I put for the buttons to work ranging from the video player itself to the speed up buttons
 
 let likeCount = 0;
-const likeBtn = document.querySelector("#like-btn");
-const likesDisplay = document.querySelector("#likes");
 
 likeBtn.addEventListener("click", () => {
   likeCount++;
   likesDisplay.textContent = likeCount;
 });
 
-// Initialize video settings and event listeners
+// Similar to const I grouped all event listeners to make it more readable
 video.removeAttribute("controls");
 video.addEventListener("timeupdate", updateProgressBar);
 fullscreenBtn.addEventListener("click", toggleFullscreen);
@@ -29,6 +29,8 @@ muteBtn.addEventListener("click", toggleMute);
 replayBtn.addEventListener("click", replayVideo);
 speedUpBtn.addEventListener("click", toggleSpeedUp);
 
+// Play and pause function. checking if the video is paused or ended. If so, it plays the video and switches the icon to pause.
+// Otherwise, it pauses the video and switches the icon to play.
 function togglePlayPause() {
   if (video.paused || video.ended) {
     video.play();
@@ -39,11 +41,14 @@ function togglePlayPause() {
   }
 }
 
+// Video progress bar updating the % of video it is at, The value is multiplied by 100 to convert it into a percentage,
+// and the width of the progress bar is updated to show how far along the video is.
 function updateProgressBar() {
   const value = (video.currentTime / video.duration) * 100;
   progressBar.style.width = value + "%";
 }
 
+// Full screen function to switch between full and normal viewing
 function toggleFullscreen() {
   if (!document.fullscreenElement) {
     if (video.requestFullscreen) {
@@ -53,12 +58,13 @@ function toggleFullscreen() {
   } else {
     if (document.exitFullscreen) {
       document.exitFullscreen();
+      // When exiting full screen mode still shows icon
     }
     fullscreenImg.src =
       "https://img.icons8.com/ios-glyphs/30/full-screen--v1.png";
   }
 }
-
+// Video muting/unmuting basically updating to show on and off when clicked
 function toggleMute() {
   if (video.muted) {
     video.muted = false;
@@ -69,6 +75,8 @@ function toggleMute() {
   }
 }
 
+// If selected it resets video time to 0, I added pause to ensure that
+// if replayed when video is paused it stays like that
 function replayVideo() {
   video.currentTime = 0;
   if (!video.paused) {
@@ -76,6 +84,8 @@ function replayVideo() {
   }
 }
 
+// Speed up function, I decided for 3.5 as it wasnt too fast or slow, right in the middle which I liked
+// With this in case I made sure the icon was different when selected to speed up.
 function toggleSpeedUp() {
   if (video.playbackRate === 1) {
     video.playbackRate = 3.5;
