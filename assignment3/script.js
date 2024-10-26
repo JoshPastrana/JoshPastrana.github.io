1dcd0d298442deb45ab65ab6513da61ab46bd639
JoshPastrana <s3944069@student.rmit.edu.au>
2024-10-26 17:55:34 +1100

    update.script

--- assignment3/script.js
+++ assignment3/script.js
@@ -1,23 +1,17 @@
-// Get references to all instrument elements
-const guitarImage = document.getElementById("guitar-image");
-const drumsImage = document.querySelector('img[data-instrument="drums"]');
-const pianoImage = document.querySelector('img[data-instrument="piano"]');
-const bassImage = document.querySelector('img[data-instrument="bass"]');
+// Get all instrument images
+const instrumentImages = document.querySelectorAll(".instrument-image");
 
-// Get audio elements
-const guitarAudio = document.getElementById("guitar-sound");
-const drumsAudio = document.getElementById("drums-sound");
-const pianoAudio = document.getElementById("piano-sound");
-const bassAudio = document.getElementById("bass-sound");
-
-// Add click event listeners to play respective sounds
-guitarImage.addEventListener("click", () => playSound(guitarAudio));
-drumsImage.addEventListener("click", () => playSound(drumsAudio));
-pianoImage.addEventListener("click", () => playSound(pianoAudio));
-bassImage.addEventListener("click", () => playSound(bassAudio));
+// Add click listeners to all images
+instrumentImages.forEach((image) => {
+  image.addEventListener("click", () => {
+    const instrument = image.getAttribute("data-instrument");
+    const audio = document.getElementById(`${instrument}-sound`);
+    playSound(audio);
+  });
+});
 
 // Function to play a sound
 function playSound(audio) {
-  audio.currentTime = 0; // Reset to start if clicked multiple times
+  audio.currentTime = 0; // Reset audio to the start
   audio.play();
 }
